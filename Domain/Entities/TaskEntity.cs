3547f8b10905f3065af6ea2c65eeb749eacd5a9f
Animalfox <a72e@outlook.com>
2024-06-26 07:18:24 +0300

    Make Id field of Task as init only

--- Domain/Entities/TaskEntity.cs
+++ Domain/Entities/TaskEntity.cs
@@ -2,7 +2,7 @@ namespace Todo.Domain.Entities;
 
 public class TaskEntity
 {
-    public int Id { get; set; }
+    public int Id { get; init; }
     public string? Title { get; set; }
     public string? Description { get; set; }
     public bool IsCompleted { get; set; }
