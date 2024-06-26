6690317cf8791145b1c192d63a867816f79745ed
Animalfox <a72e@outlook.com>
2024-06-26 06:52:10 +0300

    Create TaskRepository

---
+++ Infrastructure/Repositories/TaskRepository.cs
@@ -0,0 +1,24 @@
+using Todo.Domain.Interfaces;
+
+namespace Todo.Infrastructure.Repositories;
+
+public class TaskRepository : ITaskRepository
+{
+    private readonly List<Task> _tasks = [];
+
+    public Task GetById(int id)
+    {
+        return _tasks.FirstOrDefault(t => t.Id == id) ?? throw new InvalidOperationException();
+    }
+
+    public void Save(Task task)
+    {
+        // Simulated saving to a datastore
+        _tasks.Add(task);
+    }
+
+    public IEnumerable<Task> GetAll()
+    {
+        return _tasks;
+    }
+}
